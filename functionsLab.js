7ee41fa88a5d2b729189c0a2695453884aeaf41c
theor-2006 <124737786+theor-2006@users.noreply.github.com>
2024-01-11 23:10:46 +0000

    Code Nation 01/11/24

--- functionsLab.js
+++ functionsLab.js
@@ -1,28 +1,51 @@
 // 1. Write a function that:
 //   - Console.logs 'Hello World'
+function helloWorld() {
+  console.log("Hello World");
+}
 
 // 2. Write a function with 1 parameter that:
 //  - Console.logs whatever you want it to say.
 //  - This would be a good time to use string interpolation!
+function saySomething(something) {
+  console.log("My name is Theo");
+}
 
 // 3. Write a function with 1 parameter that:
 //  - Prints a random number between 1 and whatever number is for the provided argument. 
 //  - You might need to search how to generate a random number.
 //  - You might need to search how to round a number up.
+function randomNumber(num) {
+  console.log(Math.random()*num);
+}
 
 // 4. Write a function with 3 parameters that:
 //  - Takes the product of the first two parameters and raises them to the power of the third parameter.
 //  - Returns the solution.
 //  - Example: Passing this function 1, 2, 3 should give you back the answer to (1 * 2)^3, which is 8. 
 //  - You may need to search how to raise the power of a number!
+function paramatersThree(num1, num2, num3) {
+  console.log(Math.pow(num1 * num2, num3));
+}
 
 // 5. Write a function with 1 parameter that:
 //  - Converts Celsius to Fahrenheit.
 //  - The formula to convert Celsius to Fahrenhiet: F = (C * 1.8) + 32
+  function convert(C) {
+    F = (C * 1.8) + 32
+    console.log(F);
+  }
 
 // 6. Write a function with 1 parameter that:
 //  - Takes a word and consoles the letters in alphabetical order.
 //  - You'll need to break the word apart, sort the letters, and bring them back together for this!
+function wordConvert(hello) {
+  let word = "hello";
+  let wordArray = word.split("");
+  let wordSort = wordArray.sort();
+  let wordJoin = wordSort.join("");
+  console.log(wordJoin);
+}
 
 // BONUS FUNCTIONS!
 // 7. Write a function that:
--- functionsPractice.js
+++ functionsPractice.js
@@ -4,13 +4,18 @@
 //   - Multiplies them together, and 
 //   - Logs them to the console.
 
+
 // 1. DECLARE & NAME THE FUNCTION
 //   - What should I name this function?
-
+function multiplyTwoNums(num1, num2) {
+  console.log(num1*num2);
+}
 // 2. PARAMETERS
 //   - How many parameters will I need?
 //   - How do I know this?
 //   - What should I call them?
+
+
 
 // 3. BODY
 //   - What needs to happen to my parameters to give me the result I want?
@@ -18,10 +23,16 @@
 
 // 4. FUNCTION CALL
 //   - Call the function and pass through arguments to see if the function works.
-
+  multiplyTwoNums (10, 45);
 
 
 // CODE SOLO
 // 5. Write a function that will do the following:
 //   - If the correct password “October” is entered, print the message “Success!” to the console
 //   - Else, print “Try again!” if any other password is entered.
+function passwordCheck (){
+  if password === "October" {
+    console.log (“Success!”)
+  }else {
+    connsole.log(“Try again!”)
+}}
